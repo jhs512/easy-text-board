0db8ae354489ec232d0f4634ea187081b5916887
jhs512 <jangka512@gmail.com>
2020-11-04 11:40:52 +0900

    서비스와 DAO를 컨테이너에서 공급받는 구조로 변경

--- src/com/sbs/example/easytextboard/service/ArticleService.java
+++ src/com/sbs/example/easytextboard/service/ArticleService.java
@@ -2,14 +2,15 @@ package com.sbs.example.easytextboard.service;
 
 import java.util.List;
 
+import com.sbs.example.easytextboard.container.Container;
 import com.sbs.example.easytextboard.dao.ArticleDao;
 import com.sbs.example.easytextboard.dto.Article;
 
 public class ArticleService {
 	private ArticleDao articleDao;
-	
+
 	public ArticleService() {
-		articleDao = new ArticleDao();
+		articleDao = Container.articleDao;
 	}
 
 	public int write(int memberId, String title, String body) {
